a0f3ac51240d2adfa5c6dd415a37e462b7f14125
AChristoff <aleksov101@gmail.com>
2020-06-19 15:27:27 +0300

    form validations

--- src/app/forms/register-reactive/register-reactive.component.ts
+++ src/app/forms/register-reactive/register-reactive.component.ts
@@ -8,14 +8,33 @@ import {FormBuilder, Validators} from '@angular/forms';
 })
 export class RegisterReactiveComponent implements OnInit {
   form;
+
   constructor(private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      fullName: ['', Validators.required, Validators.pattern ],
-      email: ['', Validators.required, Validators.email ],
+      fullName: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/[A-Z][a-z]+\s[A-Z][a-z]+/)
+        ]
+      ],
+      email: [
+        '',
+        Validators.required,
+        Validators.email
+      ],
     });
+  }
+
+  onRegister() {
+    console.log(this.form);
+  }
+
+  get f() {
+    return this.form.controls;
   }
 
 }
