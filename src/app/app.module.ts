d72de48346d517b9dae67a54ddcf52855d7925e8
AChristoff <aleksov101@gmail.com>
2020-06-30 23:11:25 +0300

    create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppComponent} from './app.component';
 import {NavigationComponent} from './navigation/navigation.component';
+
+import {HomeComponent} from './home/home.component';
 import {SigninComponent} from './authentication/signin/signin.component';
 import {SignupComponent} from './authentication/signup/signup.component';
-import {HomeComponent} from './home/home.component';
+
 import {DropdownDirective} from './navigation/dropdown.directive';
 import {CollapseDirective} from './navigation/collapse.directive';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AuthService} from './authentication/auth.service';
-import {ToastrModule} from 'ngx-toastr';
 import {JwtInterceptorService} from './interceptors/jwt-interceptor.service';
+import {FurnitureService} from './furniture/furniture.service';
+
+import {ToastrModule} from 'ngx-toastr';
+
+import {CreateFurnitureComponent} from './furniture/create-furniture/create-furniture.component';
+import {FurnitureAllComponent} from './furniture/furniture-all/furniture-all.component';
+import {FurnitureDetailsComponent} from './furniture/furniture-details/furniture-details.component';
+import {FurnitureUserComponent} from './furniture/furniture-user/furniture-user.component';
 
 @NgModule({
   declarations: [
@@ -26,17 +35,23 @@ import {JwtInterceptorService} from './interceptors/jwt-interceptor.service';
     HomeComponent,
     DropdownDirective,
     CollapseDirective,
+    FurnitureAllComponent,
+    CreateFurnitureComponent,
+    FurnitureDetailsComponent,
+    FurnitureUserComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
     ToastrModule.forRoot(),
   ],
   providers: [
     AuthService,
+    FurnitureService,
     {provide: [HTTP_INTERCEPTORS], useClass: JwtInterceptorService, multi: true},
   ],
   bootstrap: [AppComponent]
---
+++ src/app/furniture/furniture.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+const createUrl = 'http://localhost:5000/furniture/create';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FurnitureService {
+  constructor(private http: HttpClient) { }
+
+  createFurniture(data) {
+    return this.http.post  (createUrl, data);
+  }
+}
